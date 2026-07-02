627ee8f80ec176798c3def42190bb2b81627ff7d
Maksim Belfer <maksim.belfer@lamoda.ru>
2026-07-03 00:10:16 +0300

    feat: added ability to read from words.txt

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+)
 
 func main() {
-	fmt.Println(("Initial counter cli"))
+
+	data, err := os.ReadFile("./words.txt")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Data:", string(data))
+	fmt.Println("Data length is", len(data))
 }
